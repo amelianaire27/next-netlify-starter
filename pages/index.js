5bcd688c6a3036552c3d3bfc694e4fabd719353e
Amelia <amelia.rae27@gmail.com>
2021-12-22 13:52:10 -0800

    [skip ci]
    This is a really cool test

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>This is a change! For Netlify Drag + Drop!</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
